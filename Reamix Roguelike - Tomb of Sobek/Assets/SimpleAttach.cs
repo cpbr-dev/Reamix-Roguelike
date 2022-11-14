f306e45e0cf4bac9cd0966fc0c2af6b8eafcff5c
MunsterMc <30962666+MunsterMc@users.noreply.github.com>
2022-11-14 21:13:49 +0100

    New test scene, joystick movement, crossbow attach to hand (needs fixing)

---
+++ Reamix Roguelike - Tomb of Sobek/Assets/SimpleAttach.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+    
+public class SimpleAttach : MonoBehaviour
+{
+    private Interactable interactable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactable = GetComponent<Interactable>();
+    }
+
+    // Update is called once per frame
+    private void OnHandHoverBegin(Hand hand)
+    {
+        hand.ShowGrabHint();
+    }
+
+    private void OnHandHoverEnd(Hand hand)
+    {
+        hand.HideGrabHint();
+    }
+
+    private void HandHoverUpdate(Hand hand)
+    {
+        GrabTypes grabType = hand.GetGrabStarting();
+        bool isGrabEnding = hand.IsGrabEnding(gameObject);
+        
+        //Grab object
+        if (interactable.attachedToHand == null && grabType != GrabTypes.None)
+        {
+            hand.AttachObject(gameObject, grabType);
+            hand.HoverLock(interactable);
+            hand.HideGrabHint();
+        }
+        //Release
+        else if (isGrabEnding)
+        {
+            hand.DetachObject(gameObject);
+        }
+    }
+}
