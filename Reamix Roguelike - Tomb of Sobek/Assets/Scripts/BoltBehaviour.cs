0622ade9f91baf9c5641dfc031487d369bde00ba
Munster <nael.messaoud06@gmail.com>
2022-12-15 12:09:10 +0100

    Crossbow fixed

--- Reamix Roguelike - Tomb of Sobek/Assets/Scripts/BoltBehaviour.cs
+++ Reamix Roguelike - Tomb of Sobek/Assets/Scripts/BoltBehaviour.cs
@@ -1,41 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BoltBehaviour : MonoBehaviour
 {
-    private bool didHit;
     private Rigidbody rb;
 
-    [SerializeField]
-    private float torque;
+    [SerializeField] private float torque;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    public void Fly(Vector3 force)
+    private void OnCollisionEnter(Collision collision)
     {
-        rb.isKinematic = false;
-        rb.AddForce(force, ForceMode.Impulse);
-        rb.AddTorque(transform.right * torque);
-        transform.SetParent(null);
-    }
-    void OnTriggerEnter(Collider collider)
-    {
-        if (didHit) return;
-        didHit = true;
-
-        /*if (collider.CompareTag(enemyTag))
+        if (collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("Environment"))
         {
-            var health = collider.GetComponent<HealthController>();
-            health.ApplyDamage(damage);
-        }*/
-
-        rb.velocity = Vector3.zero;
-        rb.angularVelocity = Vector3.zero;
-        rb.isKinematic = true;
-        transform.SetParent(collider.transform);
+            rb.isKinematic = true;
+            transform.parent = collision.transform;
+            gameObject.GetComponent<Collider>().enabled = false;
+            /* If it's an enemy, drop its health */
+            Destroy(this, 8f);
+        }
     }
 }
--- Reamix Roguelike - Tomb of Sobek/Assets/Scripts/Weapon.cs
+++ Reamix Roguelike - Tomb of Sobek/Assets/Scripts/Weapon.cs
@@ -1,3 +1,4 @@
+using System.Buffers;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,49 +9,13 @@ public class Weapon : MonoBehaviour
     public GameObject arrowPrefab;
     public Transform spawnPoint;
 
-    [SerializeField]
-    private float firePower = 20;
-    [SerializeField]
-    private float reloadTime;
-    private bool isReloading = false;
-
-    private GameObject currentArrow;
-
-    void Start()
-    {
-        XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
-        grabbable.activated.AddListener(Fire);
-        StartCoroutine(ReloadAfterTime());
-    }
-
-    public void Reload()
-    {
-        if (isReloading || currentArrow != null) return;
-        isReloading = true;
-        StartCoroutine(ReloadAfterTime());
-    }
-
-    private IEnumerator ReloadAfterTime()
-    {
-        yield return new WaitForSeconds(reloadTime);
-        currentArrow = Instantiate(arrowPrefab, spawnPoint);
-        currentArrow.transform.localPosition = Vector3.zero;
-        /*currentArrow.SetEnemyTag(enemyTag);*/
-        isReloading = false;
-    }
-
-    public void Fire(ActivateEventArgs arg)
+    [SerializeField] private float firePower = 20;
+    public void Fire()
     {
         Debug.Log("Firing bolt");
-        if (isReloading || currentArrow == null) return;
-        var force = spawnPoint.TransformDirection(Vector3.forward * firePower);
-        currentArrow.GetComponent<BoltBehaviour>().Fly(force);
+        var currentArrow = Instantiate(arrowPrefab, transform.position, transform.rotation);
+        currentArrow.GetComponent<Rigidbody>().velocity =  - transform.right * firePower * 15;
         currentArrow = null;
-        Reload();
     }
 
-    public bool IsReady()
-    {
-        return (!isReloading && currentArrow != null);
-    }
 }
