b861f96ba32479047a22dc92572e83ca01b7fea4
MariaFariss <maria.fariss.etu@univ-lemans.fr>
2023-01-22 12:23:08 +0100

    destroyV2

---
+++ Reamix Roguelike - Tomb of Sobek/Assets/Scripts/Destroy.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+
+public class Destroy : MonoBehaviour
+{
+        public int cubesPerAxis = 8;
+        public float delay = 1f;
+        public float force = 300f;
+        public float radius = 2f;
+
+        void Start()
+        {
+        
+    }
+
+    void Update()
+    {
+     
+        if (Input.GetKey(KeyCode.A))
+        {
+            Invoke("Main", 0f);
+        }
+            
+    }
+
+
+        void Main() {
+            for (int x = 0; x < cubesPerAxis; x++)
+            {
+                for (int y = 0; y < cubesPerAxis; y++)
+                {
+                    for (int z = 0; z < cubesPerAxis; z++)
+                    {
+                        CreateCubes(new Vector3(x, y, z));
+                    }
+                }
+            }
+        Destroy(gameObject);
+    }
+        
+        void CreateCubes(Vector3 coordinates)
+        {
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            Renderer rd = cube.GetComponent<Renderer>();
+            //rd.material.color = new Color(coordinates.x / cubesPerAxis, coordinates.y / cubesPerAxis, coordinates.z / cubesPerAxis);
+            rd.material = GetComponent<Renderer>().material;
+            cube.transform.localScale = transform.localScale / cubesPerAxis;
+            Vector3 firstCube = transform.position - transform.localScale / 2 +cube.transform.localScale /2;
+            cube.transform.position = firstCube + Vector3.Scale(coordinates, cube.transform.localScale);
+            Rigidbody rb = cube.AddComponent<Rigidbody>();
+            rb.AddExplosionForce(force, transform.position, radius);
+        }
+
+
+
+
+    }
+ 
+
+
