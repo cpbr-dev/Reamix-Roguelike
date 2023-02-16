a46da3ef99b36d3e94815d87021e8da5d13757d7
Munster <nael.messaoud06@gmail.com>
2023-02-16 12:11:33 +0100

    Door manager and opener completed

--- Reamix Roguelike - Tomb of Sobek/Assets/Scripts/Room Logic/DoorRemover.cs
+++ Reamix Roguelike - Tomb of Sobek/Assets/Scripts/Room Logic/DoorRemover.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DoorRemover : MonoBehaviour
 {
     private GameObject nearbyDoor;
+    [SerializeField] private Transform doorPos;
+    private void Start()
+    {
+        detectDoors();
+    }
 
     public void RemoveDoors()
     {
+        Debug.Log("Disabling nearbyDoor.");
         disableObject(nearbyDoor);
+        Debug.Log("Disabling self.");
         disableObject(this.gameObject);
     }
 
@@ -17,17 +25,28 @@ public class DoorRemover : MonoBehaviour
         if (door != null)
         {
             door.SetActive(false);
-        }   
+            return;
+        }
+        Debug.Log("door is null, not disabling");
     }
 
-    void OnCollisionEnter(Collision collision)
+    private void detectDoors()
     {
-        Debug.Log("Collision detected");
-        if ( collision.gameObject.CompareTag("Door") ){
-            nearbyDoor = collision.gameObject;
-            Debug.Log("Object linked : " + nearbyDoor.name);
+        Debug.Log("Searching for doors");
+        Collider[] hitColliders = Physics.OverlapSphere(doorPos.position, 1);
+        Debug.Log("Found " + hitColliders.Length + " objects nearby, checking for door tag");
+        for (int i = 0; i < hitColliders.Length; i++){
+            GameObject hitCollider = hitColliders[i].gameObject;
+            if ( hitCollider.gameObject.CompareTag("Door") && (hitCollider.transform.position != transform.position) ) {
+                Debug.Log("Found the door, setting it to nearbyDoor");
+                nearbyDoor = hitCollider.gameObject;
+            }
         }
     }
-
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(doorPos.position, 1);
+    }
 
 }
