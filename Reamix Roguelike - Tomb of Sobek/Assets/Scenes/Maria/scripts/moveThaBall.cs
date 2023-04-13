fd753bb0871cc3b6868de26fe947e95ecc6991c8
MariaFariss <102377465+MariaFariss@users.noreply.github.com>
2023-04-13 12:15:21 +0200

    commit

--- Reamix Roguelike - Tomb of Sobek/Assets/Scenes/Maria/scripts/moveThaBall.cs
+++ Reamix Roguelike - Tomb of Sobek/Assets/Scenes/Maria/scripts/moveThaBall.cs
@@ -58,10 +58,8 @@ public class moveThaBall : MonoBehaviour
 
 
 
-
-    public class BallController : MonoBehaviour
-{
-    public float speed = 10f;
+    public float speed = 5f;
+    public float jumpForce = 10f;
 
     private Rigidbody rb;
 
@@ -75,12 +73,17 @@ public class moveThaBall : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
-        
-    }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    
 }
+
 
 }
 
