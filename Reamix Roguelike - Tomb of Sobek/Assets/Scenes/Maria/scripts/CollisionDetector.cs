53dbcd164b7f97858b5ba247207d97ce1cb6d079
MariaFariss <102377465+MariaFariss@users.noreply.github.com>
2023-05-19 13:45:19 +0200

    commit

--- Reamix Roguelike - Tomb of Sobek/Assets/Scenes/Maria/scripts/CollisionDetector.cs
+++ Reamix Roguelike - Tomb of Sobek/Assets/Scenes/Maria/scripts/CollisionDetector.cs
@@ -3,25 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class CollisionDetector : MonoBehaviour
 {
-    // private void OnCollisionEnter(Collision collision)
-    // {
-        
-    //     if (collision.gameObject.CompareTag("ball"))
-    //     {
-        
-    //         Debug.Log("La boule a collisionné avec l'objet !");
-    //         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    //     }
-    // }
-
      void OnCollisionEnter(Collision collision)
     {
-        // Vérifiez si l'objet avec lequel la boule est entrée en collision a l'étiquette "Obstacle"
-        Debug.Log("La boule a collisionné avec l'objet !");
+        
         if (collision.gameObject.CompareTag("ball"))
         {
             Debug.Log("La boule a collisionné avec l'objet !");
-            // Arrêtez la scène en chargeant une nouvelle scène
             SceneManager.LoadScene("GameOverScene");
         }
     }
